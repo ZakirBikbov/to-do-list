6fa0d5acc54f9526be6f5c80d2d703bb83dcae69
Zakir Bikbov <zakir.bikbov@gmail.com>
2024-02-15 02:35:55 +0500

    Добавил модалку для внесения тасков, и сразу адаптировал под маленькие размеры экрана

--- js/main.js
+++ js/main.js
@@ -3,12 +3,29 @@ const todo = document.querySelector('.all-tasks');
 const inprogress = document.querySelector('.in-progress');
 const done = document.querySelector('.done');
 const addBtn = document.querySelector('#add-btn');
+const modalWrapper = document.querySelector('.modal-wrapper');
 
 filters.forEach(filter => {
-    filter.addEventListener('click', function () {
+    filter.addEventListener('click', () => {
         filters.forEach(function (li) {
             li.classList.remove('selected');
         });
         this.classList.add('selected');
     });
 });
+
+const myModal = () => {
+    modalWrapper.style.display = 'flex';
+    modalWrapper.addEventListener('click', (event) => {
+        // условие, чтобы modal не закрывался при нажатии на сам modal
+        if (event.target === modalWrapper) {
+            modalWrapper.style.display = 'none';
+        }
+    });
+
+}
+
+addBtn.addEventListener('click', () => {
+    myModal();
+})
+
