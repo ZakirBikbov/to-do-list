51bc1dd433b7994de2083c4a7715444c5c9a3202
Zakir Bikbov <zakir.bikbov@gmail.com>
2024-02-15 01:44:55 +0500

    Стилизовал таски, добавил адаптив

--- js/main.js
+++ js/main.js
@@ -1,4 +1,5 @@
 const todo = document.querySelector('.all-tasks');
-const inprogress = document.querySelector('.inprogress');
+const inprogress = document.querySelector('.in-progress');
 const done = document.querySelector('.done');
+const addBtn = document.querySelector('#add-btn');
 
