6f5aef8d39a4d61ba21bfd15b7ea0153418f8a5b
Zakir Bikbov <zakir.bikbov@gmail.com>
2024-02-15 02:07:49 +0500

    Реализовал переключение фильтров на уровне смены стилей

--- js/main.js
+++ js/main.js
@@ -1,5 +1,14 @@
+const filters = document.querySelectorAll('.filters li');
 const todo = document.querySelector('.all-tasks');
 const inprogress = document.querySelector('.in-progress');
 const done = document.querySelector('.done');
 const addBtn = document.querySelector('#add-btn');
 
+filters.forEach(filter => {
+    filter.addEventListener('click', function () {
+        filters.forEach(function (li) {
+            li.classList.remove('selected');
+        });
+        this.classList.add('selected');
+    });
+});
